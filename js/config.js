6f933d71006232850ebeaa8676299d8bb593894a
bor <muyuruhai@gmail.com>
2015-08-13 11:45:26 +1000

    init

--- js/config.js
+++ js/config.js
@@ -14,7 +14,8 @@ zhConfig = (function(){
             "good":{"from":15,"to":21,"text":"不错啊，战斗力很好","stamp":"stamp_3.png"},
             "normal":{"from":10,"to":14,"text":"多多练习，不然就被卖了","stamp":"stamp_3.png"},
             "bad":{"from":1,"to":9,"text":"马上就要被拐走了","stamp":"stamp_2.png"},
-            "terrible":{"from":-100,"to":0,"text":"你在山里还能上网么。。","stamp":"stamp_2.png"}
+            "terrible":{"from":-100,"to":0,"text":"你在山里还能上网么。。","stamp":"stamp_2.png"},
+            "else":{"from":-50000,"to":-100,"text":"额。。。","stamp":"stamp_2.png"}
         }
     };
     return me;
